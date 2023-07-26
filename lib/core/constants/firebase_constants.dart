63b472002408c88514414bc10c71c797c009a66f
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-26 05:57:27 +0300

    firebase collections

--- lib/core/constants/firebase_constants.dart
+++ lib/core/constants/firebase_constants.dart
@@ -1,3 +1,5 @@
 class FirebaseConstants {
-  
+  static const String usersCollection = "users";
+  static const String postsCollection = "posts";
+  static const String commentsCollection = "comments";
 }
