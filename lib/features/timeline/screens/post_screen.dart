f6a8728468107670739bd93722993da2a6bb8d51
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-27 02:19:53 +0300

    fixed issue where liking in post details doesnt update likes

--- lib/features/timeline/screens/post_screen.dart
+++ lib/features/timeline/screens/post_screen.dart
@@ -16,6 +16,7 @@ class PostScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print("rebuild");
     return Scaffold(
         appBar: AppBar(
           elevation: 0,
