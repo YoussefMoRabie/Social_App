595223f84c5e78634f27fe15eac7af446f399dd2
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-27 04:25:01 +0300

    fixed profile page

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -17,93 +17,95 @@ class ProfileScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(30),
-          child: Column(
-            children: [
-              /// -- IMAGE
-              Stack(
-                children: [
-                  SizedBox(
-                    width: 120,
-                    height: 120,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: const Image(
-                          image: AssetImage("assets/images/profile.jpg"),
-                          fit: BoxFit.cover,
-                        )),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      width: 35,
-                      height: 35,
-                      decoration: BoxDecoration(
+          child: SafeArea(
+            child: Column(
+              children: [
+                /// -- IMAGE
+                Stack(
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: ClipRRect(
                           borderRadius: BorderRadius.circular(100),
-                          color: Palette.primary),
-                      child: const Icon(
-                        LineAwesomeIcons.alternate_pencil,
-                        color: Colors.black,
-                        size: 20,
+                          child: const Image(
+                            image: AssetImage("assets/images/profile.jpg"),
+                            fit: BoxFit.cover,
+                          )),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: Palette.primary),
+                        child: const Icon(
+                          LineAwesomeIcons.alternate_pencil,
+                          color: Colors.black,
+                          size: 20,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              const Text("Meow was here",
-                  style: TextStyle(color: Palette.white, fontSize: 28)),
-              const Text("Bio", style: TextStyle(color: Palette.white)),
-              const SizedBox(height: 20),
-
-              /// -- BUTTON
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    context.push("/profile/update");
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Palette.primary,
-                      side: BorderSide.none,
-                      shape: const StadiumBorder()),
-                  child: const Text("Edit Profile",
-                      style: TextStyle(color: Palette.white)),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 30),
-              const Divider(),
-              const SizedBox(height: 10),
-
-              /// -- MENU
-              ProfileMenuWidget(
-                  title: "Settings",
-                  icon: LineAwesomeIcons.cog,
-                  onPress: () {}),
-              ProfileMenuWidget(
-                  title: "Billing Details",
-                  icon: LineAwesomeIcons.wallet,
-                  onPress: () {}),
-              ProfileMenuWidget(
-                  title: "User Management",
-                  icon: LineAwesomeIcons.user_check,
-                  onPress: () {}),
-              const Divider(),
-              const SizedBox(height: 10),
-              ProfileMenuWidget(
-                  title: "Information",
-                  icon: LineAwesomeIcons.info,
-                  onPress: () {}),
-              ProfileMenuWidget(
-                title: "Logout",
-                icon: LineAwesomeIcons.alternate_sign_out,
-                textColor: Colors.red,
-                endIcon: false,
-                onPress: () {
-                  ref.read(authControllerProvider.notifier).logout();
-                },
-              ),
-            ],
+                const SizedBox(height: 10),
+                const Text("Meow was here",
+                    style: TextStyle(color: Palette.white, fontSize: 28)),
+                const Text("Bio", style: TextStyle(color: Palette.white)),
+                const SizedBox(height: 20),
+          
+                /// -- BUTTON
+                SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.push("/profile/update");
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Palette.primary,
+                        side: BorderSide.none,
+                        shape: const StadiumBorder()),
+                    child: const Text("Edit Profile",
+                        style: TextStyle(color: Palette.white)),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Divider(),
+                const SizedBox(height: 10),
+          
+                /// -- MENU
+                ProfileMenuWidget(
+                    title: "Settings",
+                    icon: LineAwesomeIcons.cog,
+                    onPress: () {}),
+                ProfileMenuWidget(
+                    title: "Billing Details",
+                    icon: LineAwesomeIcons.wallet,
+                    onPress: () {}),
+                ProfileMenuWidget(
+                    title: "User Management",
+                    icon: LineAwesomeIcons.user_check,
+                    onPress: () {}),
+                const Divider(),
+                const SizedBox(height: 10),
+                ProfileMenuWidget(
+                    title: "Information",
+                    icon: LineAwesomeIcons.info,
+                    onPress: () {}),
+                ProfileMenuWidget(
+                  title: "Logout",
+                  icon: LineAwesomeIcons.alternate_sign_out,
+                  textColor: Colors.red,
+                  endIcon: false,
+                  onPress: () {
+                    ref.read(authControllerProvider.notifier).logout();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
